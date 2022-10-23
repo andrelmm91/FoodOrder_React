52709de3e6aee626b5f5e032c564044686c73e06
unknown <andre.lmm91@gmail.com>
2022-10-23 09:38:45 +0200

    adding an animation to the cart

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,35 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/Cart-context";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighed, setBtnIsHighlighed] = useState(false);
+
   const cartCtx = useContext(CartContext);
+
   const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${btnIsHighlighed ? classes.bump : ''}`;
+  const { items } = cartCtx;
+
+  useEffect(() => {
+    if (cartCtx.items.length === 0) {return;}
+    setBtnIsHighlighed(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighed(false);
+    }, 300);//remove animation class to be played again
+
+    return (() => {
+      clearTimeout(timer)
+    })
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
